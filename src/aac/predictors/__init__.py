cf02f79ac891dfc20fbb01f85055bba1b54d165b
Bonnie McConnell <bonniep.mcconnell@gmail.com>
2026-04-07 12:43:34 +1200

    feat: export all predictors in init

--- src/aac/predictors/__init__.py
+++ src/aac/predictors/__init__.py
@@ -1,4 +1,13 @@
+from .edit_distance import EditDistancePredictor
+from .frequency import FrequencyPredictor
+from .history import HistoryPredictor
 from .static_prefix import StaticPrefixPredictor
+from .trie import TriePrefixPredictor
 
-__all__ = ["StaticPrefixPredictor"]
-
+__all__ = [
+    "EditDistancePredictor",
+    "FrequencyPredictor",
+    "HistoryPredictor",
+    "StaticPrefixPredictor",
+    "TriePrefixPredictor",
+]
