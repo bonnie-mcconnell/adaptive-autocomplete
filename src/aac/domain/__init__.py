39fbfd3d0ba4e028749be9733ba707f1595f7b09
Bonnie McConnell <bonniep.mcconnell@gmail.com>
2026-04-10 12:29:22 +1200

    chore: update packaging, update domain init

--- src/aac/domain/__init__.py
+++ src/aac/domain/__init__.py
@@ -1,1 +1,23 @@
 
+from .history import History, HistoryEntry
+from .types import (
+    CompletionContext,
+    Predictor,
+    PredictorExplanation,
+    ScoredSuggestion,
+    Suggestion,
+    WeightedPredictor,
+    ensure_context,
+)
+
+__all__ = [
+    "CompletionContext",
+    "History",
+    "HistoryEntry",
+    "Predictor",
+    "PredictorExplanation",
+    "ScoredSuggestion",
+    "Suggestion",
+    "WeightedPredictor",
+    "ensure_context",
+]
